57db6b3ebebac6159335f1d8db19a57ffbec718d
Tehreem <tehreemzubair2004@gmail.com>
2026-03-11 07:05:02 +0500

    recursion modification 1.0

--- pythonPractice/recursion.py
+++ pythonPractice/recursion.py
@@ -1,8 +1,14 @@
-#recursion
-import sys
-def hello():
-    print("Hello")
-    hello()
-sys.setrecursionlimit(500)
-print(sys.getrecursionlimit())
-#hello()
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+#print(factorial(4))
+
+def print_num(n):
+    if n==0:
+        return 0
+    print_num(n-1)
+    print(n)
+
+print_num(10)
